74112db6d4ff0ecf4db63eaaf9d7a45b6859e8cc
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2025-02-26 19:33:10 -0300

    changes

--- src/main/java/com/br/parkegiro/api/Model/Admin/Admin.java
+++ src/main/java/com/br/parkegiro/api/Model/Admin/Admin.java
@@ -1,4 +1,21 @@
 package com.br.parkegiro.api.Model.Admin;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "admin")
 public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String name;
+    public String surname;
+    public String cpf;
+    public String phoneNumber;
+    public String email;
+    private String password;
+
+
 }
