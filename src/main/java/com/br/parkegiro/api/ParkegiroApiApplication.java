6691e32095ad32fd841fdb8529d62ab147fcd4c2
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2025-02-26 18:07:40 -0300

    initialized

---
+++ src/main/java/com/br/parkegiro/api/ParkegiroApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.parkegiro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkegiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkegiroApiApplication.class, args);
+	}
+
+}
